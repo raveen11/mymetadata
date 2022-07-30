78286ebb3895d6ff7121b57d825707047eb35dc9
raveen11 <neupane.rabin11@gmail.com>
2022-07-30 10:00:29 +0545

    fixes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 'process.env.PORT' || 5000;
+const port = 'https://raveen11.github.io/mymetadata/';
 const path = require('path');
 const fs = require('fs')
 
